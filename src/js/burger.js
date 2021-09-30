5f539feb2adc681642ffd27c28f0e8e7e7f6bb74
Pavel Petrov <doctorrosen2016@gmail.com>
2021-09-30 23:31:41 +0600

    working with menu-animation

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,13 +1,40 @@
-let menuBtn = document.querySelector(".header__burger");
-let menu = document.querySelector(".content");
+// let menuBtn = document.querySelector(".header__burger");
+// let menu = document.querySelector(".content");
+// menuBtn.addEventListener("click", () => {
 
-menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("active");
-    menu.classList.toggle("active");
+//     menuBtn.classList.toggle("active");
+//     menu.classList.toggle("active");
 
-    menu.classList.contains("active") ?
-        (menu.style.display = "grid") :
-        (menu.style.display = "none");
+//     if (menu.classList.contains("active")) {
+//         document.querySelector('header').style.overflow = "hidden";
+//         menu.style.display = "grid";
+//     } else {
+//         document.querySelector('header').style.overflow = "";
+//         menu.style.display = "none";
+//     }
+// });
+document.addEventListener("DOMContentLoaded", (e) => {
+    let body = document.body;
+    let header = document.querySelector('.header__body');
+    let menuBtn = document.querySelector(".header__burger");
+    let menu = document.querySelector(".content");
 
-    document.getElementsByTagName("body")[0].style.overflow = "hidden";
-});
+    menuBtn.addEventListener('click', () => {
+        body.classList.toggle('lock');
+        header.classList.toggle('active');
+        menu.classList.toggle('active');
+        menuBtn.classList.toggle('active');
+
+        if (menu.classList.contains('active')) {
+            menu.style.display = 'grid';
+            menu.style.animation = 'moveWindowLeft 0.3s 1';
+            menu.style.left = '0';
+        } else {
+            menu.style.animation = 'moveWindowRight 0.3s 1';
+
+            setTimeout(() => {
+                menu.style.display = 'none';
+            }, 300);
+        }
+    })
+})

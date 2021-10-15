d1123180e7e6b0589fc3a431d7345918dcb8715c
Ivan Ushmorov <ivan.ushmorov@zeiss.com>
2021-10-15 21:42:54 +0200

    moved the anitmation logic from js to scss

--- src/js/burger.js
+++ src/js/burger.js
@@ -9,29 +9,6 @@ const init_burger = () => {
 		HEADER.classList.toggle('active');
 		CONTENT.classList.toggle('active');
 		MENU_BTN.classList.toggle('active');
-
-		if (window.screen.width <= 767) {
-			if (CONTENT.classList.contains('active')) {
-				CONTENT.style.display = 'grid';
-				CONTENT.style.animation = 'moveWindowLeft 0.3s 1';
-				CONTENT.style.left = '0';
-			} else {
-				CONTENT.style.animation = 'moveWindowRight 0.3s 1';
-
-				setTimeout(() => {
-					CONTENT.style.display = 'none';
-				}, 300);
-			}
-		}
-
-		if (window.screen.width <= 1024 && window.screen.width >= 768) {
-			if (CONTENT.classList.contains('active')) {
-				CONTENT.style.animation = 'moveWindowLeft 0.3s 1';
-				setTimeout(() => {
-					CONTENT.style.left = '0';
-				}, 300);
-			}
-		}
 	});
 };
 

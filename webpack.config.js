2ec29ea65fd67c56395eb50e33c0cc8650548ca2
Pavel Petrov <doctorrosen2016@gmail.com>
2021-10-17 14:56:04 +0600

    new version of dev branch

--- webpack.config.js
+++ webpack.config.js
@@ -5,98 +5,93 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const filename = (ext) =>
-  isDev ? `[name]${[ext]}` : `[name].[contenthash]${[ext]}`;
+    isDev ? `[name]${[ext]}` : `[name].[contenthash]${[ext]}`;
 
 const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
+    context: path.resolve(__dirname, "src"),
 
-  mode: "development",
+    mode: "development",
 
-  entry: "./js/index.js",
+    entry: "./js/index.js",
 
-  output: {
-    filename: `./js/${filename(".js")}`,
-    path: path.resolve(__dirname, "app"),
-    assetModuleFilename: `[path][name][ext]`,
-  },
+    output: {
+        filename: `./js/${filename(".js")}`,
+        path: path.resolve(__dirname, "app"),
+        assetModuleFilename: `[path][name][ext]`,
+    },
 
-  devtool: "inline-source-map",
+    devtool: "inline-source-map",
 
-  devServer: {
-    port: 3000,
-  },
+    devServer: {
+        port: 3000,
+    },
 
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: "UnicornWitnesses",
-      template: path.resolve(__dirname, "src/index.html"),
-      filename: "index.html",
-      minify: {
-        collapseWhitespace: isProd,
-      },
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: `css/${filename(".css")}`,
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "src/assets/"),
-          to: path.resolve(__dirname, "app/assets/"),
-        },
-      ],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          "css-loader",
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: (resourcePath, context) => {
-                return path.relative(path.dirname(resourcePath), context) + "/";
-              },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "UnicornWitnesses",
+            template: path.resolve(__dirname, "src/index.html"),
+            filename: "index.html",
+            minify: {
+                collapseWhitespace: isProd,
             },
-          },
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    "autoprefixer"
-                  ]
-                ]
-              }
-            }
-          },
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(?:|gif|png|ico|jpg|jpeg|svg|webp)$/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(?:|woff2|woff|ttf)$/,
-        type: "asset/resource",
-      },
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `css/${filename(".css")}`,
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, "src/assets/"),
+                to: path.resolve(__dirname, "app/assets/"),
+            }, ],
+        }),
     ],
-  },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader",
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: (resourcePath, context) => {
+                                return path.relative(path.dirname(resourcePath), context) + "/";
+                            },
+                        },
+                    },
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "autoprefixer"
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(?:|gif|png|ico|jpg|jpeg|svg|webp)$/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(?:|woff2|woff|ttf)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
 };

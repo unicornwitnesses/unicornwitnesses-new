7d34c88324d9d853c6ee5d730395cafb1022c8ac
Pavel Petrov <doctorrosen2016@gmail.com>
2021-11-14 17:57:20 +0600

    complete styking speach

--- src/js/burger.js
+++ src/js/burger.js
@@ -12,4 +12,18 @@ const init_burger = () => {
 	});
 };
 
+const expandBlock = () => {
+	const textBlocks = document.querySelectorAll('.text-block__view-btn');
+	textBlocks.forEach((el) => {
+		el.addEventListener('click', (ev) => {
+			const speachText = ev.target.parentNode.querySelector('.text-block__speach');
+			speachText.classList.toggle('expand');
+			speachText.classList.contains('expand')
+				? (el.innerText = 'Показать меньше')
+				: (el.innerText = 'Показать больше');
+		});
+	});
+};
+
 document.addEventListener('DOMContentLoaded', init_burger);
+document.addEventListener('DOMContentLoaded', expandBlock);
